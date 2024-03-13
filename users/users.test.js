add3b3f48b7407735cba7b2d1fa43ef61d27d25c
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-13 12:15:56 +0000

    ticket 4 done

--- users/users.test.js
+++ users/users.test.js
@@ -1,3 +1,22 @@
 import { expect, test } from "vitest";
+import request from "supertest";
+import app from "../app";
 
-test("GET /api/health works", () => {});
+test("GET /api/health works", async () => {
+	const response = await request(app).get("/api/health");
+	// .set("Accept", "application/json");
+	// expect(response.headers["Content-Type"]).toMatch(/json/);
+	// expect(response.status).toEqual(200);
+	// expect(response.body.email).toEqual("foo@bar.com");
+	console.log(response.body);
+});
+
+// Goal: make a GET request with Supertest to the /api/health
+//
+// import the Express app from app.js (because it needs to be tested)
+// import the function from Supertest (maybe call it `request` like the documentation did)
+// within the existing test, follow the example from the documentation and:
+//    call `request` and pass in the Express app as an argument
+//    send a GET request to the /api/health endpoint
+//    await the overall expression and assign it to a `response` constant
+//    console.log `response.body` for now to at least see a result
