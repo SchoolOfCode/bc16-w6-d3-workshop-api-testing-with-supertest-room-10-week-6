48473edc2bf5253389ff32c91540e5bb4a4cb88f
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-03-13 14:11:12 +0000

    updates for ticket 6

--- users/users.test.js
+++ users/users.test.js
@@ -13,12 +13,31 @@ test("GET /api/health works", async () => {
   console.log(response.body);
 });
 
-// Goal: make a GET request with Supertest to the /api/health
-//
-// import the Express app from app.js (because it needs to be tested)
-// import the function from Supertest (maybe call it `request` like the documentation did)
-// within the existing test, follow the example from the documentation and:
-//    call `request` and pass in the Express app as an argument
-//    send a GET request to the /api/health endpoint
-//    await the overall expression and assign it to a `response` constant
-//    console.log `response.body` for now to at least see a result
+// ticket 6 here
+
+// write a test skeleton with a descriptive test name ("GET /api/users" could be a starting point)
+test("GET /api/users", async () => {
+  // run tests to make sure the skeleton passes on its own
+  // then within the test:
+  //    ARRANGE:
+  //      use the `resetUsersTable` function to reset the database table to a known state
+  //    ACT:
+  //      use Supertest to send a GET request to the `/api/users` endpoint
+  //      wait for the response
+  const response = await request(app).get("/api/users");
+  //    ASSERT:
+  //      assert that the response body is an object
+  expect(response.body).toEqual({ success: true, payload: [{}] });
+  console.log(response.body);
+  //      assert that response body.success is true
+  //      assert that response body.payload is an array
+  //      loop over the payload array. for each user object in the payload array:
+  //          assert that user object.id is a number
+  //          assert that user object.username is a string
+  //      assert that the response status code is 200
+  //      assert that there's a Content-Type response header which contains `application/json`
+  //      any other assertions that you think would be useful
+});
+
+// run tests to ensure they passes
+// temporarily break the implementation in `users/users.controller.js` to ensure test fails and then change back so that tests pass
